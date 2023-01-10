00250d70a10c3fc12a483893dfe535ba0fd5b88e
Soquat <msoquat@questax.com>
2023-01-10 13:07:32 +0100

    empty project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,32 +2,12 @@ package org.example;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileReader;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-    File file = new File("src/main/test.txt");
-    try{
-        FileWriter writer = new FileWriter("home.txt", true);
-        writer.write("Hello world!");
-        writer.close();
-
-    }
-    catch (IOException e){
-        System.out.println("went wrong");
-    }
-
-
-
-    if(file.exists()){
-        System.out.println("That file exists");
-        System.out.println(file.getAbsolutePath());
-    } else {
-        System.out.println("that file doesnt exist");
-
-
-    }
 
 
     }
