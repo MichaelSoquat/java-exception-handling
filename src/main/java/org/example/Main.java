3f9522372ae2d881e8c50d8e44fd02d8fb379d93
Soquat <msoquat@questax.com>
2023-01-10 13:29:01 +0100

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,19 @@
 package org.example;
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-import javax.sound.sampled.*;
+
 
 
 
 public class Main {
-    public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
-        Scanner scanner = new Scanner(System.in);
-        File file = new File("ice-fire.wav");
-        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioStream);
+    public static void main(String[] args) throws IOException {
 
-        String response = "";
+        JFrame frame = new JFrame();
+        frame.setSize(420,420);
+        frame.setVisible(true);
 
-
-
-        while (!response.equals("Q")) {
-            System.out.println("P= play, S = Stop, R=Reset, Q=Quit");
-            System.out.print("Enter your choice: ");
-
-            response = scanner.next();
-            response = response.toUpperCase();
-
-            switch (response) {
-                case ("P"):
-                    clip.start();
-                    break;
-                default: System.out.println("Not a valid response!");
-                case ("S"):
-                    clip.stop();
-                    break;
-                case ("R"):
-                    clip.setMicrosecondPosition(0);
-                    break;
-                case("Q"):
-                    clip.close();
-                    break;
-
-            }
-        }
 
 
 
