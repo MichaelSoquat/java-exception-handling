8480083e24a22fb56ba636d83bd19b39dd339ed9
Soquat <msoquat@questax.com>
2023-01-11 11:37:00 +0100

    looking like a pokeball

--- src/main/java/org/example/MyPanel.java
+++ src/main/java/org/example/MyPanel.java
@@ -13,7 +13,10 @@ public class MyPanel extends JPanel {
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setStroke(new BasicStroke(5));
+        g2d.setPaint(Color.red);
+        g2d.fillArc(0,0,100,100,0,180);
+        g2d.setPaint(Color.white);
+        g2d.fillArc(0,0,100,100,180,180);
 
-        g2d.drawArc(0,0,100,100,0,180);
     }
 }
